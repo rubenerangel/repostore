5f26beedf32d77475213fba336c120127fb8c53e
Ruben E. Rangel <rubenrangel@indigosnetwork.com>
2020-05-01 08:33:31 -0400

    Component to diplat data

---
+++ src/app/user-repos/user-repos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { GitRepoInfoService } from '../services/git-repo-info.service';
+
+@Component({
+  selector: 'app-user-repos',
+  templateUrl: './user-repos.component.html',
+  styleUrls: ['./user-repos.component.sass'],
+})
+export class UserReposComponent implements OnInit {
+  constructor(public gitInfoService: GitRepoInfoService) {}
+
+  ngOnInit(): void {
+    this.gitInfoService.info$.subscribe(console.log);
+  }
+}
